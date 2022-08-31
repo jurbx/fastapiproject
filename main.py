1ad358dff0588268f3b933298773f9dc907e8c67
jurb_x <drimskiy.daniki@gmail.com>
2022-08-31 20:31:49 +0200

    update cors and app middleware

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from db.base import database
 from endpoints import users, auth, jobs
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='FastApiProject')
 app.include_router(users.router, prefix='/api/users', tags=['users'])
@@ -23,6 +24,18 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['GET', 'PUT', 'POST'],
+    allow_headers=['Content-Type', 'application/xml']
+)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, host='0.0.0.0', reload=True)
