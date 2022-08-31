e331f68869cf0a76a050dd91de0da5d11af0d1da
jurb_x <drimskiy.daniki@gmail.com>
2022-08-31 20:34:30 +0200

    update cors and app middleware

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['GET', 'PUT', 'POST'],
+    allow_methods=['GET', 'PUT', 'POST', 'DELETE'],
     allow_headers=['Content-Type', 'application/json']
 )
 
