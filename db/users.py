4ff9425a36066a0a4e4b3f632c7ae3b825c0b6c6
jurb_x <drimskiy.daniki@gmail.com>
2022-09-01 08:16:48 +0200

    update db users

--- db/users.py
+++ db/users.py
@@ -8,7 +8,7 @@ users = sqlalchemy.Table(
     metadata,
     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True),
     sqlalchemy.Column('email', sqlalchemy.String, primary_key=True, unique=True),
-    sqlalchemy.Column('name', sqlalchemy.String),
+    sqlalchemy.Column('name', sqlalchemy.String, blank=True),
     sqlalchemy.Column('hashed_password', sqlalchemy.String),
     sqlalchemy.Column('is_company', sqlalchemy.Boolean),
     sqlalchemy.Column('created_at', sqlalchemy.DateTime, default=datetime.datetime.utcnow),
