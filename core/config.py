14302facfaa79a0df0ef0b2150edb1cf30486731
jurbx <74430098+jurbx@users.noreply.github.com>
2022-09-21 15:37:41 +0200

    Update config.py

--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ from starlette.config import Config
 
 config = Config('.env_dev')
 
-url = 'postgres://jgwcmktftfxhxz:fa471ac99fab8beb811a24598a5f4a101a1993f680d698f4921d18ea3960e18f@ec2-34-241-90-235.eu-west-1.compute.amazonaws.com:5432/d9dnvpsak351rc'
+url = os.environ.get('DATABASE_URL')
 url = url.split('//')[1]
 DATABASE_URL = f'postresql://{url}'
 # DATABASE_URL = config('EE_DATABASE_URL', cast=str, default='')
